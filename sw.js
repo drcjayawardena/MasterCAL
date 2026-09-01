2f91024892bb48f5bc227d1675b9e6c18ae9a9b3
drcjayawardena <drcjayawardena@gmail.com>
2026-09-01 22:14:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  * so users pick up the new version.
  ******************************************************/
 
-const CACHE_VERSION = "mastercal-v5";
+const CACHE_VERSION = "mastercal-v58";
 
 const SHELL = [
   "./",
