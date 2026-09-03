586c9303f2679690f6de769c3f223cf01db9f4ae
drcjayawardena <drcjayawardena@gmail.com>
2026-09-03 23:53:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@
 const API_URL =
   "https://script.google.com/macros/s/AKfycbzgi6Deu62PbH8oD8KI_5Elf7GIma7BwqOmJtPIb62Dp4f-7hTJ0teUx1La1T8uiD0X/exec";
 
-
 /* =========================================================
    API HELPER
    Uses text/plain body to stay a "simple" CORS request
